3a27926f2b1be2e31bfec1bfe28acda39fc8b8d5
MulithaBM <mulithabanukamoremada123@gmail.com>
2022-05-25 18:15:37 +0530

    Get topic(s) and Update topic state

--- controllers/supervisor/supervisor.js
+++ controllers/supervisor/supervisor.js
@@ -1,80 +1,76 @@
+import Topic from '../../models/supervisor/topic.js';
 import Supervisor from '../../models/supervisor/supervisor.js';
 
-import jwt from 'jsonwebtoken'; 
-import bcrypt from 'bcrypt';
-import { v4 as uuid4 } from 'uuid';
-
-const saltRounds = 10;
-
-const signUp = async (request, response) => {
+const topicById = async (request, response, next) => {
+    const topicId = request.params.id;
     try {
-        var newSupervisor = new Supervisor(request.body);
-        
-        const filter = { "email" : newSupervisor.email }
-        const exist = await Supervisor.findOne(filter);
-
-        if(exist) {
-            response.status(422).json({ error : "This email has already been used" });
-        }
-        else {
-            const id = uuid4();
-            const hashedPassword = await bcrypt.hash(newSupervisor.password, saltRounds);
-
-            newSupervisor.id = id;
-            newSupervisor.password = hashedPassword;
-
-            await newSupervisor.save().then((data) => {
-                response.status(200).json({ message : "Signup succesfull" })
-            }).catch((error) => {
-                console.log(error);
-                response.status(400).json({ error : error.message, message : "Signup failed" });
-            });
-        }
+        const topic = await Topic.findOne({ topicId : topicId });
+        request.topic = topic;
+        next();
     }
     catch(error) {
-        response.status(500).json({ error : error.message, message : "Internal erver error" });
+        response.status(400).json({ message : "Topic could not be found", error : error });
     }
 };
 
-const signIn = async (request, response) => {
-    try {
-        const { email, password } = request.body;
+const getSingleTopic = async (request, response) => {
+    response.status(200).json(request.topic);
+};
 
-        /**
-         * Check if this validation necessary
-         */
-        if(!email || !password) {
-            response.send("Email and password necessary");
+const getAllTopics = async (request, response) => {
+    const supervisorId = request.supervisorLogged[0].id;
+    const state = request.params.state;
+    try {
+        var field = await Supervisor.find({ id : supervisorId }, 'field');
+        field = field[0].field;
+
+        const filter = {
+            field : field,
         }
 
-        const filter = { email : email }
-        const supervisor = await Supervisor.findOne(filter);
-        
-        if(supervisor) {
-            if(await bcrypt.compare(password, supervisor.password)) {
-                const token = jwt.sign(
-                    { id : supervisor._id, role : "SUPERVISOR" },
-                    process.env.JWT_SS,
-                    {
-                        expiresIn : process.env.JWT_SS_LIFETIME
-                    }
-                );
-                response.status(200).json({ token : token });
-            }
-            else {
-                response.status(401).error({ message : "Invalid email or password" });
-            }
+        if(state === "accepted") {
+            filter.state = "accepted";
+        }
+        else if(state === "rejected") {
+            filter.state = "rejected";
+        }
+        else if(state === "pending") {
+            filter.state = "pending";
         }
         else {
-            response.status(401).error({ message : "Invalid email or password" });
+            response.status(400).json({ error : "Invalid state" });
+        }
+
+        try {
+            const topics = await Topic.find(filter);
+            response.status(200).json(topics);
+        }
+        catch(error) {
+            response.status(400).json({ message : "Topics not found" });
         }
     }
     catch(error) {
-        response.status(500).json({ error : error.message, message : "Internal erver error" });
+        response.status(400).json({ message : "Field not found", error : error });
     }
 };
+
+const updateState = async (request, response) => {
+    const state = request.params.state;
+    const topic = request.topic;
+    topic.state = state;
+    const updatedTopic = new Topic(topic);
+    try {
+        await updatedTopic.save();
+        response.status(200).json({ message : "Update succesfull" });
+    }
+    catch(error) {
+        response.state(400).json({ message : "Update failed", error : error });
+    }
+}
 
 export {
-    signUp,
-    signIn
-};
+    topicById,
+    getSingleTopic,
+    getAllTopics,
+    updateState
+}
